45b2cb308f8a75c753ba536d30dde6fdeafeb7ec
felix <felixmdev@gmail.com>
2016-03-21 17:57:37 -0700

    nav bar fixes, table into grid

--- frontserver/index.js
+++ frontserver/index.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var request = require('request');
+var fs = require('fs');
+var jsgo = require('jsgo');
+var dbKey = require('./dbKey.js');
+var pg = require('pg');
+var conString = dbKey();
 
-request('', function (error, response, body) {
-    console.log(body) // Show the HTML for the Google homepage.
-})
-
-
+//rendering /index.html at '/' route;
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
+//socket on connection
 io.on('connection', function(socket){
   socket.emit('idAssign', socket.id);
+
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT weapon FROM fire', function(err,results){
+      if(err) {
+        return console.error('Your query is flawed');
+      }
+      // console.log(results);
+      socket.emit('frontpgstats', results);
+      });
+    });
+  });
 
   // socket.on('move', function(data) {
   //   io.emit('move', data);
@@ -30,7 +45,6 @@ io.on('connection', function(socket){
   // });
 
 
-});
 
 http.listen(8888, '0.0.0.0', function() {
   console.log('Listening to port:  ' + 8888);
--- frontserver/public/js/scripts.js
+++ frontserver/public/js/scripts.js
@@ -3,9 +3,16 @@ $(function() {
   var socket = io();
   socket.on('connectedUsers', function(data) {
   });
+  //
+  // socket.on('idAssign', function(data) {
+  //   id = data;
+  // });
+  // listening for frontpgstats;
+  socket.on('frontpgstats', function(results){
+    // console.log(results);
+    for(var i = 0; i < results.length; i++){
+      console.log(results[i]);
+    }
 
-  socket.on('idAssign', function(data) {
-    id = data;
-  });
-
+  })
 });
