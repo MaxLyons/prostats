db0b1173b602fc4c44fe8f343c8b13c7b54e84bc
Andy Kwan <zelthrox@gmail.com>
2016-03-21 17:39:29 -0700

    added statistic page

--- frontserver/public/js/scripts.js
+++ frontserver/public/js/scripts.js
@@ -8,6 +8,7 @@ $(function() {
     id = data;
   });
 
+/* LIVE STATS TAB
   function addData (stat){
     console.log("I AM READING THE DATA");
   };
@@ -17,8 +18,8 @@ $(function() {
   });
 
 
-
-  $(".liveStatTableTabs").on('click',function(){
+  // CLICKING TABS ON TABLE TO CHANGE DATA
+  $(".liveTableTabs").on('click',function(){
     var table = $(this)[0].id;
     $("*[data-tableNum=" + table + "]").removeClass('fade').siblings().addClass('fade');
 
@@ -31,8 +32,8 @@ $(function() {
 
   });
 
-
-  $('#container').highcharts({
+  //CREATE BUBBLE GRAPHS FOR PLAYERS KILLS
+  $('#liveGraphContainer').highcharts({
 
     chart: {
       type: 'bubble',
@@ -93,7 +94,70 @@ $(function() {
 
   });
 
+LIVE STATS TAB */
 
+/* STATIC STATS STARTS HERE */
+  // CLICKING TABS ON TABLE TO CHANGE DATA
+  $(".statTableTabs").on('click',function(){
+    var table = $(this)[0].id;
+    $("*[data-tableNum=" + table + "]").removeClass('fade').siblings().addClass('fade');
+
+    socket.emit('wantTable', table);
+
+    socket.on('getTable', function(stat){
+      console.log('addData');
+      addData(stat);
+    });
+  });
+
+
+  //STATIC GRAPH
+  $('#statGraphContainer').highcharts({
+    title: {
+      text: 'Monthly Average Temperature',
+      x: -20 //center
+    },
+    subtitle: {
+      text: 'Source: WorldClimate.com',
+      x: -20
+    },
+    xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+      title: {
+        text: 'Temperature (°C)'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valueSuffix: '°C'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+    series: [{
+      name: 'Tokyo',
+      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+    }, {
+      name: 'New York',
+      data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+    }, {
+      name: 'Berlin',
+      data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+    }, {
+      name: 'London',
+      data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+    }]
+  });
 
 });
 
