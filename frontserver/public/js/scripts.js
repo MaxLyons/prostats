3b36bbd882100e59980181460cc0d66f2c7c24a4
Andy Kwan <zelthrox@gmail.com>
2016-03-23 13:12:51 -0700

    adjusteted live page and slider

--- frontserver/public/js/scripts.js
+++ frontserver/public/js/scripts.js
@@ -28,7 +28,29 @@ $(function() {
 
   });
 
+  $(".prevPage").on('click',function(){
+    // get the current width element css 
+    var currPositionPx = $(this).closest(".slideContainer").css("left").toString();
+    var currPosition = Number(currPositionPx.substring(0, currPositionPx.length - 2));
+    // calculate the width of the screen
+    var winWidthPx = $(this).closest(".page").css("width");
+    var winWidth = Number(winWidthPx.substring(0, winWidthPx.length -2));
+    //replace the css width element
+    var newPostion = currPosition + winWidth;
+    $(this).closest(".slideContainer").css('left', newPostion);
+  });
 
+  $(".nextPage").on('click',function(){
+    // get the current width element css 
+    var currPositionPx = $(this).closest(".slideContainer").css("left").toString();
+    var currPosition = Number(currPositionPx.substring(0, currPositionPx.length - 2));
+    // calculate the width of the screen
+    var winWidthPx = $(this).closest(".page").css("width");
+    var winWidth = Number(winWidthPx.substring(0, winWidthPx.length -2));
+    //replace the css width element
+    var newPostion = currPosition - winWidth;
+    $(this).closest(".slideContainer").css('left', newPostion);
+  });
 /* LIVE STATS TAB
   function addData (stat){
     console.log("I AM READING THE DATA");
