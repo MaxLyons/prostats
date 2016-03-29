783876dfa1b735e8cf2bab58a439de2462531451
Max Danielson-Lyons <danielsonlyonsmax@gmail.com>
2016-03-29 12:49:16 -0700

    finished backend server

--- statserver/index.js
+++ statserver/index.js
@@ -1,4 +1,4 @@
-}var express = require('express');
+var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var http = require('http').Server(app);
@@ -10,30 +10,16 @@ var conString = "";
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.post('/', function(req, res) {
-    console.log(req.body);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
-pg.connect(conString, function(err, client, done) {
-
-    if(err) {
-      return console.error('error fetching client from pool', err);
-    }
-    done();
-    // client.query('CREATE TABLE rounds(id serial PRIMARY KEY, match_id int, round_num INTEGER, team_won TEXT)', function(err, result) {
-    //
+    // client.query('CREATE TABLE rounds(id TEXT PRIMARY KEY, game_id TEXT, round_num INTEGER, team_won TEXT, winning_side TEXT, t_score INTEGER, ct_score INTEGER)', function(err, result) {
     //     if(err) {
     //       return console.error('error running query', err);
     //     }
     //     console.log("hey");
     //   });
 
-    // client.query('CREATE TABLE matches(id serial PRIMARY KEY, match_date timestamp without time zone, event text, winner TEXT)', function(err, result) {
-    //
-    //     if(err) {
-    //       return console.error('error running query', err);
-    //     }
-    //     console.log("hey");
-    //   });
 
     // client.query('ALTER TABLE rounds ADD FOREIGN KEY(match_id) REFERENCES matches(id)', function(err, result) {
     //
@@ -43,7 +29,7 @@ pg.connect(conString, function(err, client, done) {
     //     console.log("hey");
     //   });
 
-    // client.query('CREATE TABLE player_rounds(id serial PRIMARY KEY, round_id INTEGER, player_id INTEGER, kills INTEGER, deaths INTEGER, assists INTEGER, damage INTEGER)', function(err, result) {
+    // client.query('CREATE TABLE player_rounds(id serial PRIMARY KEY, round_id INTEGER, player_id TEXT, kills INTEGER, deaths INTEGER, assists INTEGER, damage INTEGER)', function(err, result) {
     //
     //     if(err) {
     //       return console.error('error running query', err);
@@ -59,106 +45,207 @@ pg.connect(conString, function(err, client, done) {
     //     console.log("hey");
     //   });
 
-    client.query('ALTER TABLE team_match ADD FOREIGN KEY(team) REFERENCES teams(name);', function(err, result) {
+    // client.query('CREATE TABLE team_match(id serial PRIMARY KEY, team INTEGER, match INTEGER)', function(err, result) {
+    //
+    //     if(err) {
+    //       return console.error('error running query', err);
+    //     }
+    //     console.log("hey");
+    //   });
 
-        if(err) {
-          return console.error('error running query', err);
-        }
-        console.log("hey");
+    // client.query('ALTER TABLE players ADD FOREIGN KEY(team) REFERENCES teams(id);', function(err, result) {
+    //
+    //     if(err) {
+    //       return console.error('error running query', err);
+    //     }
+    //     console.log("hey");
+    //   });
+    // client.query('CREATE TABLE games(match_id TEXT PRIMARY KEY, event TEXT, team1 TEXT, team2 TEXT, team1_score INTEGER, team2_score INTEGER, winner TEXT)', function(err, result) {
+    //     if(err) {
+    //       return console.error('error running query', err);
+    //     }
+    //     console.log("hey");
+    //   });
+    // pg.connect(conString, function(err, client, done) {
+    //   if(err) {
+    //     return console.error('error fetching client from pool', err);
+    //   }
+    //   done();
+    //   client.query('CREATE TABLE games(match_id TEXT PRIMARY KEY, event TEXT, team1 INTEGER, team2 INTEGER, team1_score INTEGER, team2_score INTEGER, winner TEXT)', function(err, result) {
+    //       if(err) {
+    //         return console.error('error running query', err);
+    //       }
+    //       console.log("hey");
+    //     });
+    // });
+
+
+    function randomString(length, chars) {
+        var result = '';
+        for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+        return result;
+    }
+
+    io.on('connection', function(socket){
+
+      socket.on('add team', function(data) {
+        pg.connect(conString, function(err, client, done) {
+          if(err) {
+            return console.error('error fetching client from pool', err);
+          }
+          done();
+          client.query("INSERT INTO teams (name, psr, logo) VALUES ('" + data.name + "', 0, '" + data.logo + "');", function(err, result) {
+            if(err) {
+              return console.error('error running query', err);
+            }
+            io.emit('team added', data.name);
+            client.query("SELECT * FROM teams", function(err, result) {
+              var teamArray = result.rows;
+              for (var i = 0; i < teamArray.length; i ++) {
+                var team = teamArray[i];
+                var teamObject = {id: team.id, name: team.name, logo: team.logo};
+                io.emit('populate select', teamObject);
+              }
+            });
+          });
+        });
       });
 
+      socket.on('add player', function(data) {
+        pg.connect(conString, function(err, client, done) {
+          if(err) {
+            return console.error('error fetching client from pool', err);
+          }
+          done();
+          client.query("INSERT INTO players (steam_id, team, elo, picture, alias) VALUES ('" + data.steam_id + "', '" + data.team_id + "', 0, '" + data.picture + "', '" + data.name + "');", function(err, result) {
+            if(err) {
+              return console.error('error running query', err);
+            }
+            io.emit('player added', data.name);
+          });
+        });
+      });
 
-});
-// var rounds = [];
-// fs.readFile('demo.dem', function(err, data) {
-//   var demo = new jsgo.Demo();
-//   // new jsgo.Demo().on('server_info', function(event) {
-//   //   var map = event.mapName;
-//   // }).parse(data);
-//
-//   function spew(event_name){
-//     var roundNumber = 1;
-//     demo.on(event_name, function(event) {
-//       var teamArray = [];
-//       var playerArray = [];
-//       console.log("------------\nEvent:  " + event_name);
-//       var teams = this.getTeams();
-//       for (var i = 0; i < teams.length; i++) {
-//         var team = teams[i];
-//         var side = team.getSide();
-//         var name = team.getClanName();
-//         var score = team.getScore();
-//
-//         var players = team.getPlayers(this);
-//         if (side == 'TERRORIST' || side == 'CT') {
-//           for (var x = 0; x < players.length; x++) {
-//             var player = players[x];
-//             if (!player.isFakePlayer() && !player.isHLTV()) {
-//               var killsArray = player.getData().m_iMatchStats_Kills;
-//               var assistsArray = player.getData().m_iMatchStats_Assists;
-//               var deathsArray = player.getData().m_iMatchStats_Deaths;
-//               var damagesArray = player.getData().m_iMatchStats_Damage;
-//               var kills = 0;
-//               var assists = 0;
-//               var deaths = 0;
-//               var damages = 0;
-//               for (var kill in killsArray) {
-//                 kills += killsArray[kill];
-//               }
-//               for (var assist in assistsArray) {
-//                 assists += assistsArray[assist];
-//               }
-//               for (var death in deathsArray) {
-//                 deaths += deathsArray[death];
-//               }
-//               for (var damage in damagesArray) {
-//                 damages = damagesArray[damage];
-//               }
-//               var playerObject = {name: player.getName(), guid: player.getGuid(), kills: kills, deaths: deaths, assists: assists, roundKills: player.getRoundKills(), damage: damages};
-//               playerArray.push(playerObject);
-//             }
-//           }
-//           var won = false;
-//           if (event.winner == 2 && side == 'TERRORIST') {
-//             score += 1;
-//             won = true;
-//           } else if (event.winner == 3 && side == 'CT') {
-//             score += 1;
-//             won = true;
-//           }
-//           var teamObject = {name: name, side: side, score: score, won: won};
-//           teamArray.push(teamObject);
-//         }
-//       }
-//       var roundObject = {round: roundNumber, teams: teamArray, players: playerArray};
-//       rounds.push(roundObject);
-//       roundNumber += 1;
-//       console.log(teamArray);
-//     });
-//   }
-//   spew('game.round_end');
-//
-//   demo.parse(data);
-//   // console.log(rounds);
-// });
+      pg.connect(conString, function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        client.query("SELECT * FROM teams", function(err, result) {
+          var teamArray = result.rows;
+          for (var i = 0; i < teamArray.length; i ++) {
+            var team = teamArray[i];
+            var teamObject = {id: team.id, name: team.name, logo: team.logo};
+            io.emit('populate select', teamObject);
+          }
+        });
+        done();
+        client.query("SELECT * FROM player_rounds", function(err, result) {
+          io.emit('populate players', result.rows);
+        });
 
-// io.on('connection', function(socket){
-//   socket.emit('idAssign', socket.id);
+    });
 
-  // socket.on('move', function(data) {
-  //   io.emit('move', data);
-  // });
+      socket.on('add game', function(gameData) {
+        pg.connect(conString, function(err, client, done) {
+          if(err) {
+            return console.error('error fetching client from pool', err);
+          }
+          var gameId = randomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+          var map = "";
+          fs.readFile('demo.dem', function(err, data) {
+            var gameDone = false;
+            var event_name = 'game.round_end';
+            var demo = new jsgo.Demo();
+                demo.on('server_info', function(event) {
+                  map = event.mapName;
+                  client.query("INSERT INTO games(match_id, event, team1, team2, map) VALUES ( '" + gameId + "', '" + gameData.title + "', '" + gameData.team1 + "', '" + gameData.team2 + "', '" + map + "');", function(err, result) {
+                    if(err) {
+                      return console.error('error running query', err);
+                    }
+                      console.log("Lol");
+                  });
+                })
+                var roundNumber = 1;
+                demo.on(event_name, function(event) {
+                  var roundId = randomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+                  console.log("-------------");
+                  var teams = this.getTeams();
+                  for (var i = 0; i < teams.length; i++) {
+                    var team = teams[i];
+                    var side = team.getSide();
+                    var name = team.getClanName();
+                    var score = team.getScore();
+                    var players = team.getPlayers(this);
+                    if (!(name == undefined) && !(name == "") && (side == 'TERRORIST' || side == 'CT')) {
+                      for (var x = 0; x < players.length; x++) {
+                        var player = players[x];
+                        if (!(player == undefined) && !player.isFakePlayer() && !player.isHLTV()) {
+                          player.getName();
+                          player.getRoundKills();
+                          var assists = player.getData().m_iMatchStats_Assists;
+                          var damages = player.getData().m_iMatchStats_Damage;
+                          var deaths = player.getData().m_iMatchStats_Deaths;
+                          var damageArray = [];
+                          var deathArray = [];
+                          var assistArray = [];
+                          for (var damage in damages) {
+                            damageArray.push(damages[damage]);
+                          }
+                          for (var death in deaths) {
+                            deathArray.push(deaths[death]);
+                          }
+                          for (var assist in assists) {
+                            assistArray.push(assists[assist]);
+                          }
+                          var roundDamage = damageArray[damageArray.length - 1];
+                          var roundDeath = deathArray[deathArray.length - 1];
+                          var roundAssist = assistArray[assistArray.length - 1];
+                          var steamId = player.getGuid();
+                          steamId = "STEAM_0:" + steamId.substring(8);
+                          if (roundAssist == undefined) {
+                            roundAssist = 0;
+                          }
+                          client.query("INSERT INTO player_rounds (round_id, player_id, kills, deaths, assists, damage, alias) VALUES ( '" + roundId + "', '" + steamId + "', '" + player.getRoundKills() + "', '" + roundDeath + "', '" + roundAssist + "', '" + roundDamage + "', '" + player.getName() + "');", function(err, result) {
+                            if(err) {
+                              return console.error('error running query', err);
+                            }
+                          });
+                        }
+                      }
+                    }
+                  }
+                  if (event.player_count == 10 && !(this.getTeams()[2].data.m_szClanTeamname == undefined) && !(this.getTeams()[3].data.m_szClanTeamname == undefined)) {
+                    var winningClan = this.getTeams()[event.winner].data.m_szClanTeamname;
+                    var winningSide = "";
+                    var ctScore = this.getTeams()[3].getScore();
+                    var tScore = this.getTeams()[2].getScore();
+                    if (event.winner == 2) {
+                      winningSide = "Terrorist";
+                      tScore += 1;
+                    } else if (event.winner == 3) {
+                      winningSide = "CT";
+                      ctScore += 1;
+                    }
+                    console.log(winningClan + " Won! They are " + winningSide);
+                    console.log("CT score: " + ctScore);
+                    console.log("T score: " + tScore);
+                    client.query("INSERT INTO rounds(id, game_id, round_num, team_won, winning_side, t_score, ct_score ) VALUES ('" + roundId + "', '" + gameId + "', " + roundNumber + ", '" + winningClan + "', '" + winningSide + "', " + tScore + ", " + ctScore + ");", function(err, result) {
+                      if(err) {
+                        return console.error('error running query', err);
+                      }
+                      done();
+                    });
+                    if (ctScore )
+                    roundNumber += 1;
+                  }
+                });
+                demo.parse(data);
+              });
+        });
+        io.emit('game added', gameData.title);
+      });
+  });
 
-  // socket.on('disconnect', function() {
-  //   var client = allClients.indexOf(socket);
-  //   allClients.splice(allClients.indexOf(socket), 1);
-  //   activeUsers.splice(client , 1);
-  //   io.emit('connectedUsers', io.engine.clientsCount);
-  //   io.emit('online', activeUsers);
-  // });
-
-
-// });
 http.listen(3000, function() {
   console.log('Listening to port:  ' + 3000);
 });
--- statserver/public/js/scripts.js
+++ statserver/public/js/scripts.js
@@ -1,11 +1,58 @@
 $(function() {
-  var id = "";
   var socket = io();
   // socket.on('connectedUsers', function(data) {
   // });
+
   //
-  // socket.on('idAssign', function(data) {
-  //   id = data;
-  // });
+  $('#team').submit(function(){
+    var team = {name: $('#team_name').val(), logo: $('#logo_url').val()}
+    socket.emit('add team', team);
+    $('#team_name').val('');
+    $('#logo_url').val('');
+    return false;
+  });
+
+  $('#player').submit(function(){
+    var player = {team_id: $('#team_list').val(), name: $('#player_name').val(), steam_id: $('#steam_id').val(), picture: "placeholder"}
+    socket.emit('add player', player);
+    $('#player_name').val('');
+    $('#steam_id').val('');
+    $('#picture_url').val('');
+    return false;
+  });
+
+  $('#game').submit(function(){
+    var game = {title: $('#title').val(), team1: $('#team1_name').val(), team2: $('#team2_name').val()}
+    socket.emit('add game', game);
+     $('#title').val('');
+    return false;
+  });
+
+  socket.on('populate select', function(data) {
+    var select = '';
+    select += "<option value='" + data.id + "' data-icon='" + data.logo + "' class=''>" + data.name + "</option>";
+    $("#team_list").append(select);
+    $("#team1_name").append(select);
+    $("#team2_name").append(select);
+    $('select').material_select();
+  });
+
+  socket.on('populate players', function(data) {
+    console.log(data);
+    $("#player-list").append(data);
+  });
+
+
+  socket.on('team added', function(data) {
+    Materialize.toast(data + " added succesfully!", 4000);
+  });
+
+  socket.on('player added', function(data) {
+    Materialize.toast(data + " added succesfully!", 4000);
+  });
+
+  socket.on('game added', function(data) {
+    Materialize.toast(data + " added succesfully!", 4000);
+  });
 
 });
